401df29ee10a33a3928abd05d34ec09cce1d6d5c
Hopson97 <mhopson@hotmail.co.uk>
2017-08-27 10:06:58 +0100

    able to load any file

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,26 +1,45 @@
 #include "Application.h"
 
 #include <iostream>
+#include <fstream>
+
+
 
 int main(int argc, char** argv)
 {
-    Config config;
-    if (argc == 1)
+    Config configFile;
+
+    std::cout << "Trying to load image...\n";
+
+    std::ifstream inFile("config.txt");
+    if (!inFile.is_open())
     {
-        std::cout << "No arguments, using default (World Map)\n";
-        config.image.loadFromFile("res/world_map_large.png");
-        config.width    = config.image.getSize().x;
-        config.height   = config.image.getSize().y;
+        std::cerr << "Unable to open config, using default.\n";
+        configFile.image.loadFromFile("res/world_map_large.png");
     }
     else
     {
-        std::string file = argv[1];
-        std::cout << "Using " << file << "\n";
-        config.image.loadFromFile(file);
-        config.width    = config.image.getSize().x;
-        config.height   = config.image.getSize().y;
+        std::string file;
+        inFile >> file;
+        if (!configFile.image.loadFromFile("res/" + file))
+        {
+            std::cerr << "Unable to open \"" << file << "\", using default.\n";
+            configFile.image.loadFromFile("res/world_map_large.png");
+        }
+        else
+        {
+            std::cout << "Success! Using " << file << "!\n";
+        }
     }
 
-    Application app(config);
+    std::cout   << "\nI tried to make it work with cmd args, but .dll errors happened\n"
+                << "If you want change the map, add an image to the res folder and\n"
+                << "change the image name in \"config.txt\". Enjoy!\n"
+                << "Note: Your map must be green and blue!\n";
+
+    configFile.width    = configFile.image.getSize().x;
+    configFile.height   = configFile.image.getSize().y;
+
+    Application app(configFile);
     app.run();
 }
